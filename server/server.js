6983d106b56d394f4f62a5d9d047b3ac61ff7e07
Faakhir30 <zahidfaakhir@gmail.com>
2023-08-03 02:19:02 +0500

    added login/signup api's

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dontenv from "dotenv";
 import helmet from "helmet";
-
+import userRoutes from "./routes/userRoutes.js"
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/gereral.js";
 import managementRoutes from "./routes/management.js";
 import ticketingRoutes from "./routes/ticketing.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 // CONFIGURATION
 dontenv.config();
@@ -21,20 +23,18 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(cookieParser());
 
 // ROUTES
 
+app.use('/api/users', userRoutes);
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/ticketing", ticketingRoutes);
 
 const port = process.env.PORT || 9000;
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+connectDB()
   .then(() => {
     app.listen(port, () => {
       console.log(`server running sucessfully on port: ${port}`);
